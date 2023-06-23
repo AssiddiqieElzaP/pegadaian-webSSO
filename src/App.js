dd4f348e49edab8690de7b97f96227efa5d2365a
selamathariyadi <selamath6@gmail.com>
2023-06-23 16:05:26 +0700

    function auth login

--- src/App.js
+++ src/App.js
@@ -9,6 +9,41 @@ import PersetujuanUserBackup from "./pages/Persetujuan/PersetujuanUserBackup";
 import Login from '../src/login/index'
 
 const App = () => {
+  // function Riderect() {
+  //   const location = useLocation();
+  //   const searchParam = new URLSearchParams(location.search);
+  //   const codes = searchParam.get("code");
+
+  //   useEffect(() => async () => {
+  //     // const redirect = `htpp://localhost:3000` === 'admin' ? REACT_APP_SSO_REDIRECT_URI :
+  //     if (!localStorage.getItem("token")) {
+  //       const url = `http://10.254.1.180:8081/hcis/oauth/authorize?client_id=delegasilocal&response_type=code&redirect_uri=http://localhost:3000&state=12345`;
+  //       window.location.href = url;
+  //       console.log("test lagi");
+  //       console.log(codes);
+  //       try {
+  //         // await axios
+  //         //   .post(`http://localhost:8080/api/v1/auth-sso/code`, {
+  //         //     code: codes
+  //         //   })
+  //         //   .then((res) => {
+  //         //     const data = res.data
+  //         //     localStorage.setItem("token", "ini coba");
+  //         //     console.log("coba gembel");
+  //         //     console.log(data);
+  //         //   });
+  //       } catch (error) {
+  //         console.log("error mulu");
+  //       }
+  //       console.log("test", codes);
+  //     }
+
+  //     // console.log("1");
+  //     // console.log("ini coba");
+  //     // console.log("test", location);
+  //     // console.log("test lagi", codes);
+  //   });
+  // }
   return (
     <>
     <ToastContainer theme='colored' position='center'></ToastContainer>
